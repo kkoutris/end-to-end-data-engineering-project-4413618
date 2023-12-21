f26d47465648fff1ca8ed666b74a2ef060edf9a7
kkoutris <kkoutris4@gmail.com>
2023-12-21 17:01:02 +0100

    Add job and schedule

--- dagster_orchestration/dagster_orchestration/__init__.py
+++ dagster_orchestration/dagster_orchestration/__init__.py
@@ -1,5 +1,23 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import (
+    AssetSelection,
+    Definitions,
+    ScheduleDefinition,
+    define_asset_job,
+    load_assets_from_modules,
+)
 
 from .assets import resources
 
-defs = Definitions(assets=load_assets_from_modules( [assets]), resources=resources)
+all_assets = load_assets_from_modules([assets])
+
+# define a job that will materialize the assets
+big_star_job = define_asset_job("big_star_job", selection=AssetSelection.all())
+
+# ScheduleDefinition the job it should run and a cron schedule of how frequently to run it
+update_job_schedule = ScheduleDefinition(
+    job=big_star_job,
+    cron_schedule="0 * * * *",  # every hour
+)
+
+
+defs = Definitions(assets=all_assets, resources=resources,jobs=[big_star_job], schedules=[update_job_schedule])
